cd9d7588267964c8fd93de3831220a0436aee907
Potnuru Karthik <2320040019@klh.edu.in>
2025-12-02 16:23:54 +0530

    Create testbench.v

---
+++ Behavioral-Modelling/testbench.v
@@ -0,0 +1,16 @@
+`timescale 1ns/100ps
+module testbench;
+reg a,b;
+wire y_and,y_or,y_nand,y_nor,y_xor,y_xnor;
+
+behavioral_gates uut(a,b,y_and,y_or,y_nand,y_nor,y_xor,y_xnor);
+
+initial begin 
+
+a=0;b=0;#10;
+a=0;b=1;#10;
+a=1;b=0;#10;
+a=1;b=1;#10;
+
+end
+endmodule
