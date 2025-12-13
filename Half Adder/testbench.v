d3d37c41ed71aa6640255692077e3623afc8c237
Potnuru Karthik <2320040019@klh.edu.in>
2025-12-13 18:22:43 +0530

    Create testbench.v

---
+++ Half Adder/testbench.v
@@ -0,0 +1,16 @@
+`timescale 1ns/100ps
+
+module halfAdder_tb;
+reg a,b;
+wire sum,cout;
+
+half_adder uut(.a(a),.b(b),.sum(sum),.cout(cout));
+initial begin 
+
+a=0;b=0;#10;
+a=0;b=1;#10;
+a=1;b=0;#10;
+a=1;b=1;#10;
+
+end
+endmodule
