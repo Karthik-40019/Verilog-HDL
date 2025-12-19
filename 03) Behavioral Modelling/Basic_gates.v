3b4d627c2f97c327aeda0b22dd7050822c6523b2
Potnuru Karthik <2320040019@klh.edu.in>
2025-12-19 17:47:30 +0530

    Update Basic_gates.v

--- 03) Behavioral Modelling/Basic_gates.v
+++ 03) Behavioral Modelling/Basic_gates.v
@@ -1,1 +1,17 @@
+module behavioral_gates(
+input a,b,
+output reg y_and,y_or,y_nand,y_nor,y_xor,y_xnor
+);
+
+always @(*) begin 
+
+y_and = a&b;
+y_or = a|b;
+y_nand = ~(a&b);
+y_nor = ~(a|b);
+y_xor = a^b;
+y_xnor = ~(a^b);
+
+end
+endmodule
 
