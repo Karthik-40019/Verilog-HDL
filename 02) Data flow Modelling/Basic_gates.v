8bac30def7d26fc594f8b6a31c1218e21a0ec09d
Potnuru Karthik <2320040019@klh.edu.in>
2025-12-19 17:46:12 +0530

    Update Basic_gates.v

--- 02) Data flow Modelling/Basic_gates.v
+++ 02) Data flow Modelling/Basic_gates.v
@@ -1,1 +1,13 @@
+module gates_dataflow(
+input a,b,
+output y_and,y_or,y_nand,y_nor,y_xor,y_xnor
+);
 
+assign y_and = a&b;
+assign y_or = a|b;
+assign y_nand = ~(a&b);
+assign y_nor = ~(a|b);
+assign y_xor = a^b;
+assign y_xnor = ~(a^b);
+
+endmodule
