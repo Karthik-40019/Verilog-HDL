ac5d0b5d6df2b8541f0a0ff44ba0c56f1aa7e47e
Potnuru Karthik <2320040019@klh.edu.in>
2025-12-31 19:50:03 +0530

    Create mux_tb.v

---
+++ 18) 4 : 1 MUX/mux_tb.v
@@ -0,0 +1,19 @@
+`timescale 1ns/100ps
+
+module mux_4x1_tb;
+reg [3:0] a;
+reg [1:0] s;
+wire y;
+
+mux_4x1 uut(a,s,y);
+initial begin 
+a=4'b1010;
+
+s=2'b00; #10;
+s=2'b01; #10;
+s=2'b10; #10;
+s=2'b11; #10;
+
+end
+endmodule
+
