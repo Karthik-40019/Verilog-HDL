245c33053535c0207c58d1dcd2daed5c3b6e1548
Potnuru Karthik <2320040019@klh.edu.in>
2026-01-13 19:51:03 +0530

    Create comparator.v

---
+++ 31) 16-bit ALU (Project-1)/comparator.v
@@ -0,0 +1,11 @@
+module comparator(
+input [15:0]a,b,
+output eql,grt,less
+);
+
+assign grt   = ($signed(a) > $signed(b));
+assign less  = ($signed(a) < $signed(b));
+assign eql   = (a==b);
+
+endmodule
+
