4e2e08e6d4d54794b9ea0919d44b3f85ec52c9ee
Potnuru Karthik <2320040019@klh.edu.in>
2026-01-14 17:02:20 +0530

    Update asr.v

--- 34) Arithmetic Right Shift/asr.v
+++ 34) Arithmetic Right Shift/asr.v
@@ -1,3 +1,4 @@
+//Arithmetic right shift is only used for signed numbers, else it will behave like a normal right shift 
 module arithmetic_right_shift #(parameter w)(
 input signed [w-1:0] a,
 input [2:0] n,
