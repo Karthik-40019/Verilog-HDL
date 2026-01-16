9a0468eb38d874198d0a8c0024b5ff899b2dff98
Potnuru Karthik <2320040019@klh.edu.in>
2026-01-16 22:30:23 +0530

    Update d_flipflop.v

--- 39) D Flip-flop/d_flipflop.v
+++ 39) D Flip-flop/d_flipflop.v
@@ -3,7 +3,8 @@ input d,
 input clk,rst,
 output reg q,qbar
 );
-always @(posedge clk or posedge rst) begin 
+always @(posedge clk or posedge rst) // Asynchronous reset condition 
+begin 
 if(rst) begin
   q<=1'b0;
   qbar<=1'b1;
