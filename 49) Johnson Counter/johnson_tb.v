30e7d4a5574223044ffc9ed3a50889c96d0aa96b
Potnuru Karthik <2320040019@klh.edu.in>
2026-01-21 18:00:51 +0530

    Create johnson_tb.v

---
+++ 49) Johnson Counter/johnson_tb.v
@@ -0,0 +1,15 @@
+`timescale 1ns/100ps
+module johnson_counter_tb;
+reg clk,rst;
+wire [3:0] q;
+
+johnson_counter uut(clk,rst,q);
+always #5 clk = ~clk;
+
+initial begin 
+clk=0; rst=1; #10;
+rst=0;
+
+end
+endmodule
+
