f07477682c1ce2c3439f8977bcd1e0a6d424c700
Potnuru Karthik <2320040019@klh.edu.in>
2026-01-24 18:44:02 +0530

    Create decade_tb.v

---
+++ 51) Decade Counter/decade_tb.v
@@ -0,0 +1,13 @@
+`timescale 1ns/100ps
+module decade_tb;
+reg clk,rst;
+wire [3:0] q;
+
+decade_counter uut(clk,rst,q);
+always #5 clk=~clk;
+initial begin 
+clk=0; rst=1; #10;
+rst=0;
+  
+end
+endmodule
