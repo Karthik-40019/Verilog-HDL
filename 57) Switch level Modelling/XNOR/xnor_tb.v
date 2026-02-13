a3bdec303517d9c2fbb9e0bf5c8835ca7229fea7
Potnuru Karthik <2320040019@klh.edu.in>
2026-02-13 15:37:07 +0530

    Add testbench for XNOR gate

---
+++ 57) Switch level Modelling/XNOR/xnor_tb.v
@@ -0,0 +1,15 @@
+`timescale 1ns/100ps 
+module xnor_tb;
+reg a,b;
+wire y;
+
+cmos_xnor uut(a,b,y);
+initial begin 
+
+a=0; b=0; #10;
+a=0; b=1; #10;
+a=1; b=0; #10;
+a=1; b=1; #10;
+
+end
+endmodule
