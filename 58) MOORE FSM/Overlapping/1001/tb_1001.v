cce8338062e759619560793708fb9efd217bbbbd
Potnuru Karthik <2320040019@klh.edu.in>
2026-02-13 16:54:43 +0530

    Add testbench for Moore FSM sequence detector

---
+++ 58) MOORE FSM/Overlapping/1001/tb_1001.v
@@ -0,0 +1,26 @@
+module moore_tb_1001;
+  reg clk,rst,x;
+  wire z;
+  
+  seq_detector_1001 uut(clk,rst,x,z);
+  always #5 clk=~clk;
+  initial begin 
+    clk=0; rst=1; #10;
+    rst=0;
+
+    //Sequence: 0100100101
+    x=0; #10;
+    x=1; #10;
+    x=0; #10;
+    x=0; #10;
+    x=1; #10;
+    x=0; #10;
+    x=0; #10;
+    x=1; #10;
+    x=0; #10;
+    x=1; #10;
+$finish();
+end
+endmodule
+
+    
