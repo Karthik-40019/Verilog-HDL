a8749026b7d04af9e6a8f55a576c9c4c238d07c5
Potnuru Karthik <2320040019@klh.edu.in>
2026-02-20 11:49:47 +0530

    Add testbench for nov_sequence_1101 module

---
+++ 58) MOORE FSM/Non Overlapping/1101/nov_tb_1101.v
@@ -0,0 +1,32 @@
+`timescale 1ns/100ps
+module nov_tb_1101;
+  reg clk,rst,x;
+  wire z;
+  
+  nov_sequence_1101 uut(clk,rst,x,z);
+  always #5 clk=~clk;
+  initial begin
+    clk=0;rst=1; x=0; #10;
+    rst=0; 
+    
+    //01101101011010
+    x=0;#10;
+    x=1;#10;
+    x=1;#10;
+    x=0;#10;
+    x=1;#10;
+    x=1;#10;
+    x=0;#10;
+    x=1;#10;
+    x=0;#10;
+    x=1;#10;
+    x=1;#10;
+    x=0;#10;
+    x=1;#10;
+    x=0;#10;
+  end
+endmodule 
+    
+    
+    
+    
