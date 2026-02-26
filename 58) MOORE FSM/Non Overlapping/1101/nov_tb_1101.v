e7c76571af9c03d61a091c5fdb391ced65c13082
Potnuru Karthik <2320040019@klh.edu.in>
2026-02-26 22:03:30 +0530

    Fix initial clock value in nov_tb_1101.v
    Initialize clk to 1 instead of 0 in the testbench.

--- 58) MOORE FSM/Non Overlapping/1101/nov_tb_1101.v
+++ 58) MOORE FSM/Non Overlapping/1101/nov_tb_1101.v
@@ -6,7 +6,7 @@ module nov_tb_1101;
   nov_sequence_1101 uut(clk,rst,x,z);
   always #5 clk=~clk;
   initial begin
-    clk=0;rst=1; x=0; #10;
+    clk=1;rst=1; x=0; #10;
     rst=0; 
     
     //01101101011010
