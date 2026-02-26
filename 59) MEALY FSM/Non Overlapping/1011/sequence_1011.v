91bbd8d40a58640a91f7d8b4e114448443b1a463
Potnuru Karthik <2320040019@klh.edu.in>
2026-02-26 22:23:54 +0530

    Create sequence_1011.v

---
+++ 59) MEALY FSM/Non Overlapping/1011/sequence_1011.v
@@ -0,0 +1,46 @@
+module mealy_nov_1101(
+  input clk,rst,x,
+  output z
+);
+  parameter A=2'b00;
+  parameter B=2'b01;
+  parameter C=2'b10;
+  parameter D=2'b11;
+  
+  reg[1:0] state,next_state;
+  
+  always @(posedge clk or posedge rst) begin
+    if(rst) begin
+      state<=A;
+    end
+    else 
+      state<=next_state;
+  end
+  
+  always @(*) begin 
+    case(state) 
+      A: begin
+        if(x==0) next_state=A;
+        else     next_state=B;
+      end
+      
+      B: begin
+        if(x==0) next_state=C;
+        else     next_state=B;
+      end
+      
+      C: begin
+        if(x==0) next_state=A;
+        else     next_state=D;
+      end
+      
+      D: begin
+        if(x==0) next_state=C;
+        else     next_state=A;
+      end
+      
+      default: next_state<=A;
+    endcase
+  end
+assign z=(state==D) && (x==1);  
+endmodule
